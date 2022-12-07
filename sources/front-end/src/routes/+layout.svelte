612ee273e3367536ddd0e7afc12fb72155861b78
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-12-07 19:46:30 +0100

    initial layout + styles + font
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/front-end/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Header from '$lib/controls/Header/index.svelte';
+  import Footer from '$lib/controls/Footer/index.svelte';
+</script>
+
+<Header />
+<main>
+  <slot />
+</main>
+<Footer />
