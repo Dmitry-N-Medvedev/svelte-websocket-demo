30cfaacebb79b20fca3bca2fda237be61f6d67cd
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-12-09 17:47:49 +0100

    +pub/sub
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/back-end/lib/LibWebsocketServer/messages/serializers/createServerTSMessage.mjs
@@ -0,0 +1,15 @@
+import {
+  MessageTypes,
+} from './MessageTypes.mjs';
+
+const encoder = new TextEncoder();
+
+export const createServerTSMessage = () => {
+  const messageObject = Object.freeze({
+    type: MessageTypes.TS,
+    payload: Date.now(),
+  });
+  const messageJSON = JSON.stringify(messageObject);
+
+  return encoder.encode(messageJSON);
+};
