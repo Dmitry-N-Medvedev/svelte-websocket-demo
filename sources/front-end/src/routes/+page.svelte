ed83429fa9b5da4798b17f7e3f11111a85284304
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-12-11 16:35:17 +0100

    tuned scale3d args
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/front-end/src/routes/+page.svelte
+++ sources/front-end/src/routes/+page.svelte
@@ -104,7 +104,7 @@
     filter: drop-shadow(0 0 1.5rem var(--theme-black));
     transition: all var(--transition-duration) var(--transition-timing-function);
 
-    transform: scale3d(1.1, 1.1, 1.1);
+    transform: scale3d(1.0, 1.0, 1.0);
   }
 
   #money {
