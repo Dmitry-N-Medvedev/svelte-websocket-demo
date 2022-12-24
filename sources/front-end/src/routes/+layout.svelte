892b984cb89943f610029491574354323c5fb9df
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-12-24 14:40:17 +0100

    added Ldr
    loads and unloads wsWorker
    
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/front-end/src/routes/+layout.svelte
+++ sources/front-end/src/routes/+layout.svelte
@@ -8,58 +8,67 @@
     onMount,
     onDestroy,
   } from 'svelte';
-  import {
-    WSClient,
-  } from '$lib/modules/ws/index.mjs';
+  // import {
+  //   WSClient,
+  // } from '$lib/modules/ws/index.mjs';
   import Header from '$lib/controls/Header/index.svelte';
   import Footer from '$lib/controls/Footer/index.svelte';
   import {
     WsStoreAdapter,
   } from '$lib/modules/wsStoreAdapter/index.mjs';
-  // import ServiceWorker from '../service-worker/index.js';
+  import {
+    Ldr,
+  } from '$lib/modules/ldr/ldr.mjs';
 
-  // console.log({ ServiceWorker });
 
-  /** @type {WSClient} */
-  // @ts-ignore
-  let wsClient = null;
+  /** @type {Ldr | null} */
+  let ldr = null;
+  // /** @type {WSClient} */
+  // // @ts-ignore
+  // let wsClient = null;
   /** @type {WsStoreAdapter} */
   // @ts-ignore
   let wsStoreAdapter = null;
-
-  const registerSW = async () => {
-    if ('serviceWorker' in navigator) {
-      try {
-        addEventListener('load', async() => {
-          const registrationResult = await navigator.serviceWorker.register('../service-worker/index.js');
-
-          console.log({ registrationResult });
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  const workersConfig = {
+    wsWorker: {
+      url: 'ws://127.0.0.1:9090/',
+    },
   };
+
+  // const registerSW = async () => {
+  //   if ('serviceWorker' in navigator) {
+  //     try {
+  //       addEventListener('load', async() => {
+  //         const registrationResult = await navigator.serviceWorker.register('../service-worker/index.js');
+
+  //         console.log({ registrationResult });
+  //       });
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  // };
 
   onMount(async () => {
     if (IsInBrowser === true) {
-      await registerSW();
+      // @ts-ignore
+      ldr = new Ldr(workersConfig);
+
+      await ldr?.start();
 
       wsStoreAdapter = new WsStoreAdapter();
       wsStoreAdapter.start();
       // @ts-ignore
-      wsClient = new WSClient(`ws://127.0.0.1:9090/`);
-      wsClient.start();
+      // wsClient = new WSClient(`ws://127.0.0.1:9090/`);
+      // wsClient.start();
     }
   });
 
-  onDestroy(() => {
+  onDestroy(async() => {
     if (IsInBrowser === true) {
-      if (wsClient) {
-        wsClient.stop();
-  
-        wsClient = null;
-      }
+      await ldr?.stop();
+
+      ldr = null;
 
       if (wsStoreAdapter) {
         wsStoreAdapter.stop();
