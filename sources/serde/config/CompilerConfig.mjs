7ed04ac79d4bd3b56e43abd8f7aa8cb8cb14f9a6
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-12-26 12:18:02 +0100

    generate ts + mjs from flatbuffers IDL
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/serde/config/CompilerConfig.mjs
@@ -0,0 +1,16 @@
+export const CompilerConfig = Object.freeze({
+  compileTo: ['ts'],
+  outputRootDir: './compiled',
+  rootFile: './schema/main.fbs',
+  languages: {
+    ts: {
+      generatorOptions: [
+        '--natural-utf8',
+        '--gen-all',
+        '--gen-mutable',
+      ],
+      preProcess: null,
+      postProcess: (await import(new URL('../processors/ts/postProcess.mjs', import.meta.url))).postProcess,
+    },
+  },
+});
