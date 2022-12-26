dfdd2c8ec9564faf27e720b6a55bdead98c90f17
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2022-12-26 15:16:02 +0100

    added --warnings-as-errors
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/serde/config/CompilerConfig.mjs
+++ sources/serde/config/CompilerConfig.mjs
@@ -8,6 +8,7 @@ export const CompilerConfig = Object.freeze({
         '--natural-utf8',
         '--gen-all',
         '--gen-mutable',
+        '--warnings-as-errors',
       ],
       preProcess: null,
       postProcess: (await import(new URL('../processors/ts/postProcess.mjs', import.meta.url))).postProcess,
